b8d507a45bfe766ee80ac46f406d7c90525997dc
ltdagabriel <gabriel_choptian@hotmail.com>
2018-08-29 09:10:10 -0300

    Start Phaser Game JAM

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('fundo', 'assets/fundo.jpg');
+}
+
+function create () {
+    this.add.image(400, 300, 'fundo');
+
+
+    emitter.startFollow(logo);
+}
